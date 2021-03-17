49f8f4ac758434e92b912102e86899a7ed96fcdd
Joseph Schwartz <joey12345345@gmail.com>
2021-03-17 18:40:53 -0400

    Fix coordinate error in testMain

--- testMain.cpp
+++ testMain.cpp
@@ -8,7 +8,7 @@ int main()
     areaSetup();
     player* p1 = player::getPlayer();
     p1->setPlayerName();
-    area* startRoom = area::getArea(3, 4);
+    area* startRoom = area::getArea(2, 3);
     startRoom->enterArea();
     while (true)
     {
